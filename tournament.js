c6dd01ba47ca72c32644fb9937ebc854410ba717
Puaz <puazboy@gmail.com>
2017-01-31 17:51:05 -0600

    Version 0.0

--- tournament.js
+++ tournament.js
@@ -1,3 +1,9 @@
+function Fighter(name,strength,speed,agility){
+	this.name = name;
+	this.strength = strength;
+	this.speed = speed;
+	this.agility = agility;
+}
 document.write("<h1>Bienvenido al torneo de Artes Marciales</h1>");
 var name = prompt("Cual es tu nombre peleador?");
 document.write (name + " se ha inscrito al torneo ");
